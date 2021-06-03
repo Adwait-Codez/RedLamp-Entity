fc746b0dc1520e5b005795d36565c2d4b1f50858
Adwait Creationz <adwait.34@gmail.com>
2021-06-03 14:33:26 +0530

    Add project files.

---
+++ Entity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Godot;
+
+namespace RedLamp_Entity
+{
+    public class Entity
+    {
+        int id;
+        String type;
+        Vector3 position;
+        Vector3 velocity;
+        float yaw;
+        float pitch;
+        bool onGround=true;
+        float height;
+        float width;
+        Dictionary<int, String> effects;
+        // 0 = held item, 1-4 = armor slot
+        String[] equipment = new String[5];
+        String heldItem; // shortcut to equipment[0]
+        bool isValid = true;
+        Array metadata;
+    }
+}
